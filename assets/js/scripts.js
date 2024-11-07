e5c991ce1d6837806eff79a36188b7a26a321560
shoteose <143038290+shoteose@users.noreply.github.com>
2024-11-07 21:03:26 +0000

    Elimina corrigido, dividi scripts para melhor organização

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,128 +1,13 @@
-
-let jogoId; // variavel do id do jogo
-
-// quando abre a modal vai buscar o id que está relacionado e guarda no jogoId
-//(pego do projeto Ligas Futebol de TW)
-const modalEliminar = document.getElementById('modalEliminar');
-if (modalEliminar) {
-    modalEliminar.addEventListener('show.bs.modal', function (event) {
-        const button = event.relatedTarget;
-        jogoId = button.getAttribute('data-id');
-    });
-}
-
-function deleteGame() {
-    // Existe algum id? Se sim elimina
-    if (jogoId) {
-
-        // esconder a modal, para melhor estetica
-        const modalInstance = bootstrap.Modal.getInstance(modalEliminar);
-        modalInstance.hide();
-
-        //toast a dizer que eliminou
-        Toastify({
-            text: "Jogo eliminado com sucesso",
-            duration: 2000,
-            close: true,
-            gravity: "top",
-            backgroundColor: "linear-gradient(200deg, rgba(255,0,0,1) 0%, rgba(255,33,0,1) 50%, rgba(221,255,0,1) 86%)"
-        }).showToast();
-
-        // Redireciona para a URL de eliminar após 1 segundos
-        setTimeout(function () {
-            window.location.href = './jogo/delete/' + jogoId;
-        }, 1000);
-    }
-}
-
 //-------------------------------------------------------------------------------------------------
-
-let generoId; // variavel do id do genero
-
-// quando abre a modal vai buscar o id que está relacionado e guarda no generoID
-//(pego do projeto Ligas Futebol de TW)
-const modalEliminarGe = document.getElementById('modalEliminarGe');
-if (modalEliminarGe) {
-    modalEliminarGe.addEventListener('show.bs.modal', function (event) {
-        const button = event.relatedTarget;
-        generoId = button.getAttribute('data-id');
-    });
-}
-
-function deleteGenero() {
-    // Existe algum id? Se sim elimina
-    if (generoId) {
-
-        // esconder a modal, para melhor estetica
-        const modalInstance = bootstrap.Modal.getInstance(modalEliminarGe);
-        modalInstance.hide();
-
-        //toast a dizer que eliminou
-        Toastify({
-            text: "Gênero eliminado com sucesso",
-            duration: 1000,
-            close: true,
-            gravity: "top",
-            backgroundColor: "linear-gradient(to right, #ff0000, #ff0000)"
-        }).showToast();
-
-        // Redireciona para a URL de eliminar após 1 segundos
-        setTimeout(function () {
-            window.location.href = './genero/delete/' + generoId;
-        }, 1000);
-    }
-}
-
-
-//---------------------------------------------------------------------------------------------
-
-let publicadoraID; // variavel do id do genero
-
-// quando abre a modal vai buscar o id que está relacionado e guarda no generoID
-//(pego do projeto Ligas Futebol de TW)
-const modalEliminarPu = document.getElementById('modalEliminarPu');
-if (modalEliminarGe) {
-    modalEliminarGe.addEventListener('show.bs.modal', function (event) {
-        const button = event.relatedTarget;
-        publicadoraID = button.getAttribute('data-id');
-    });
-}
-
-function deletePublicadora() {
-    // Existe algum id? Se sim elimina
-    if (publicadoraID) {
-
-        // esconder a modal, para melhor estetica
-        const modalInstance = bootstrap.Modal.getInstance(modalEliminarPu);
-        modalInstance.hide();
-
-        //toast a dizer que eliminou
-        Toastify({
-            text: "Publicadora eliminada com sucesso",
-            duration: 1000,
-            close: true,
-            gravity: "top",
-            backgroundColor: "linear-gradient(to right, #ff0000, #ff0000)"
-        }).showToast();
-
-        // Redireciona para a URL de eliminar após 1 segundos
-        setTimeout(function () {
-            window.location.href = './publicadora/delete/' + publicadoraID;
-        }, 1000);
-    }
-}
-
-
-//---------------------------------------------------------------------------------------------
-
 
 // Função para detectar o fim da animação
 document.addEventListener("DOMContentLoaded", function () {
+
     const title = document.getElementById('tituloIndex');
     const titleCriadores = document.getElementById('tituloCriadores');
-    console.log("o titulo do index deu:" + title);
-    console.log("o titulo do criadores deu:" + titleCriadores);
+
     if (titleCriadores) {
+
         titleCriadores.addEventListener("animationend", function () {
             console.log("help bro");
             document.getElementById('textosBotao').classList.add('supriseModaFoca');
@@ -134,24 +19,8 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-
 });
 
 //-----------------------------------------------------------------------------------
 
-// metodo de pesquisa, esconde os que não tem as letras/palavras no nome
-
-document.getElementById('searchBar').addEventListener('input', function () {
-    const searchValue = this.value.toLowerCase();
-    const jogos = document.querySelectorAll('.jogo-item');
-
-    jogos.forEach(jogo => {
-        const nome = jogo.querySelector('.jogo-nome').textContent.toLowerCase();
-        if (nome.includes(searchValue)) {
-            jogo.style.display = 'block';
-        } else {
-            jogo.style.display = 'none';
-        }
-    });
-});
 
