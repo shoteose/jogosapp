be6b4b19277f003c5d66c44b0588dc1161008a34
mchuguinho <hugodinis2001@gmail.com>
2024-11-14 16:03:16 +0000

    Made it more responsive FAKKKKKKKK

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -15,30 +15,29 @@
 </head>
 
 <body id="index">
-
   <div class="container text-games text-center">
     <div class="row">
       <div class="col-12">
         <p class="line-1 anim-typewriter" id="tituloIndex">GamesList</p>
       </div>
     </div>
+
     <div class="row mt-3" id="botoes">
-      <div class="col-12 ms-5 d-flex flex-column align-items-center">
-        <a href="<?php echo $url_alias;?>/jogo" class="btn btn-md btn-custom">Ver Jogos</a>
-        <a href="<?php echo $url_alias;?>/genero" class="btn btn-md btn-custom">Ver Gêneros</a>
-        <a href="<?php echo $url_alias;?>/publicadora" class="btn btn-md btn-custom">Ver Publicadoras</a>
+      <div class="col-12 col-md-6 col-lg-4 mb-3 d-flex flex-column align-items-center">
+        <a href="<?php echo $url_alias;?>/jogo" class="btn btn-md btn-custom mb-2">Ver Jogos</a>
+        <a href="<?php echo $url_alias;?>/genero" class="btn btn-md btn-custom mb-2">Ver Gêneros</a>
+        <a href="<?php echo $url_alias;?>/publicadora" class="btn btn-md btn-custom mb-2">Ver Publicadoras</a>
         <a href="<?php echo $url_alias;?>/criadores" class="btn btn-md btn-custom">Ver Criadores</a>
       </div>
     </div>
   </div>  
 
-
+  <!-- Scripts -->
   <script src="<?php echo $url_alias ?>/assets/js/scripts.js"></script>
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
     crossorigin="anonymous"></script>
-
 </body>
+
 
 </html>
--- app/views/publicadora/index.php
+++ app/views/publicadora/index.php
@@ -6,9 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Listagem de Gêneros</title>
   <link rel="stylesheet" href="assets/css/style.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 </head>
 
@@ -18,21 +21,36 @@
 
     <div class="d-flex flex-wrap justify-content-between mb-3">
       <a href="<?php echo $url_alias; ?>/" class="btn btn-secondary btn-sm mb-2">Voltar</a>
-      <a href="<?php echo $url_alias; ?>/publicadora/create" class="btn btn-success btn-sm mb-2">Adicionar Publicadora</a>
+      <a href="<?php echo $url_alias; ?>/publicadora/create" class="btn btn-success btn-sm mb-2">Adicionar
+        Publicadora</a>
     </div>
 
-    <div class="list-group">
-      <?php foreach ($data['publicadoras'] as $publicadora) { ?>
-        <div class="bg-dark list-group-item d-flex flex-column flex-md-row justify-content-between align-items-start">
-          <span class="mb-2 mb-md-0"><?php echo $publicadora['nome']; ?></span>
-          <span class="mb-2 mb-md-0"><?php echo $publicadora['pais']; ?></span>
-          <div class="d-flex flex-wrap gap-2">
-            <a href="./publicadora/get/<?php echo $publicadora['id']; ?>" class="btn btn-sm btn-outline-secondary">Ver Jogos</a>
-            <a data-bs-toggle="modal" data-bs-target="#modalEliminarPu" data-id="<?php echo $publicadora['id']; ?>" class="btn btn-sm btn-outline-danger">Delete</a>
-          </div>
-        </div>
-      <?php } ?>
-    </div>
+    <table class="table table-dark table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Nome</th>
+          <th scope="col">País</th>
+          <th scope="col">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($data['publicadoras'] as $publicadora) { ?>
+          <tr>
+            <td><?php echo $publicadora['nome']; ?></td>
+            <td><?php echo $publicadora['pais']; ?></td>
+            <td>
+              <div class="d-flex gap-2">
+                <a href="./publicadora/get/<?php echo $publicadora['id']; ?>" class="btn btn-sm btn-outline-secondary">Ver
+                  Jogos</a>
+                <a data-bs-toggle="modal" data-bs-target="#modalEliminarPu" data-id="<?php echo $publicadora['id']; ?>"
+                  class="btn btn-sm btn-outline-danger text-right">Delete</a>
+              </div>
+            </td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+
   </div>
 
   <!-- Modal de Eliminar -->
@@ -54,8 +72,10 @@
     </div>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNgyH6KEfElwpcl5EiyJ3jShkn1AM2YuDhCfwfBBDFqz9EufPDA6wH8LusnaGG+"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhG81r6Qq8ZTfC2m5K68K2a8a6w5VjE55rHRFVLM7xk3I6ABe9I1F2BY9HBr"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
+    integrity="sha384-eMNgyH6KEfElwpcl5EiyJ3jShkn1AM2YuDhCfwfBBDFqz9EufPDA6wH8LusnaGG+"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+    integrity="sha384-cVKIPhG81r6Qq8ZTfC2m5K68K2a8a6w5VjE55rHRFVLM7xk3I6ABe9I1F2BY9HBr"></script>
   <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
   <script src="<?php echo $url_alias ?>/assets/js/scriptsPu.js"></script>
 </body>
